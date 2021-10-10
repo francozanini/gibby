8b0dfe1d05628876b82e6a3f136a2f8bd8a323c4
francozanini <francozanini1995@gmail.com>
2021-10-10 18:02:23 -0300

    (feature) added embedResponse in mapCommand

--- src/main/java/io/franco/gibby/commands/MapsCommand.java
+++ src/main/java/io/franco/gibby/commands/MapsCommand.java
@@ -3,6 +3,9 @@ package io.franco.gibby.commands;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.Color;
+import io.franco.gibby.models.ApexMap;
 import io.franco.gibby.models.MapRotationDto;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
@@ -11,6 +14,8 @@ import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.time.Instant;
 
 @Component
 public class MapsCommand implements SlashCommand {
@@ -46,12 +51,23 @@ public class MapsCommand implements SlashCommand {
                 .retrieve()
                 .bodyToMono(MapRotationDto.class)
                 .block();
-
-        event.
+    
+        final ApexMap currentMap = mapRotationDto.getBattleRoyale().getCurrent();
+        final ApexMap nextMap = mapRotationDto.getBattleRoyale().getNext();
+        
+        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                                               .color(Color.BLUE)
+                                               .title("Map rotation")
+                                               .author("Gibby", null, null)
+                                               .addField("current", currentMap.getMap().concat(" ").concat(currentMap.getRemainingTimer()), false)
+                                               .addField("next", nextMap.getMap(), false)
+                                               .image(currentMap.getAsset())
+                                               .timestamp(Instant.now())
+                                               .build();
 
         return event
                 .reply()
                 .withEphemeral(true)
-                .withContent(mapRotationDto.getBattleRoyale().getCurrent().getMap());
+                .withEmbeds(embed);
     }
 }
